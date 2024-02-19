a7116d716d48f0be9ad7ba7a5933920e12bcc1c9
Luis Henrique Geanini <luishenriquegeanini@gmail.com>
2024-02-19 16:19:53 -0300

    Fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter as FontSans } from 'next/font/google';
 
 // UTILS
 import { cn } from '@/lib/utils';
-
-export const fontSans = FontSans({
-    subsets: ['latin'],
-    variable: '--font-sans',
-});
 
 // STYLES
 import '@/styles/globals.css';
@@ -20,12 +14,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: IChildrenProps) => {
     return (
         <html lang="pt-BR" suppressHydrationWarning>
-            <body
-                className={cn(
-                    'min-h-screen bg-background font-sans antialiased',
-                    fontSans.variable
-                )}
-            >
+            <body className={cn('min-h-screen bg-background font-sans antialiased')}>
                 {children}
             </body>
         </html>
