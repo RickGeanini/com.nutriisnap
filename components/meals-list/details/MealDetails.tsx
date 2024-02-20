5c989ac74808e590e18d2738e59e38e8bb1ff9da
Luis Henrique Geanini <luishenriquegeanini@gmail.com>
2024-02-20 15:02:25 -0300

    Init details

--- components/meals-list/details/MealDetails.tsx
+++ components/meals-list/details/MealDetails.tsx
@@ -1,11 +1,8 @@
+import Image from 'next/image';
+
 // COMPONENTS
-import {
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-} from '@/components/ui/drawer';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+import { DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle } from '@/components/ui/drawer';
 
 // INTERFACES
 import { IMeal } from '@/interfaces/meals';
@@ -16,14 +13,37 @@ interface IMealDetailsComponentProps {
 }
 // MEAL DETAILS COMPONENT
 const MealDetailsComponent = ({ meal }: IMealDetailsComponentProps) => {
+    /* Utils */
+    const renderedIngredients = (
+        <ul className="list-disc pl-8">
+            {meal.ingredients.map(ingredient => {
+                return (
+                    <li className="text-sm text-primary" key={ingredient}>
+                        {ingredient}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+
     /* Render */
     return (
         <DrawerContent className="bg-white">
-            <DrawerHeader>
-                <DrawerTitle>{meal.name}</DrawerTitle>
-                <DrawerDescription>This action cannot be undone.</DrawerDescription>
-            </DrawerHeader>
-            <DrawerFooter></DrawerFooter>
+            <div className="flex gap-4">
+                {/* <AspectRatio ratio={16 / 9} className="rounded-ss-lg rounded-se-lg bg-muted w-96"> */}
+                {/* <Image
+                    alt={meal.name}
+                    className="rounded-ss-lg rounded-se-lg object-cover size-4"
+                    fill
+                    src={meal.image}
+                /> */}
+                {/* </AspectRatio> */}
+                <div className="p-4 flex flex-col gap-4">
+                    <p className="text-2xl text-primary font-semibold">{meal.name}</p>
+                    <p className="text-2xl text-primary font-semibold">{meal.create_date}</p>
+                    {renderedIngredients}
+                </div>
+            </div>
         </DrawerContent>
     );
 };
